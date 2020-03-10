7ad4ea88ea90d59751573aa10ed266a5e3f2f538
Sidney San Martín <sidney@s4y.us>
2020-03-10 04:56:29 -0400

    WIP

--- sw.js
+++ sw.js
@@ -1,45 +1,3 @@
-// const cache_name = "cache";
-// const cache_urls = "offline.html";
-
-// self.addEventListener("install", e => {
-//   e.waitUntil(
-//     caches.open(cache_name)
-//       .then(cache => cache.add(offline_url))
-//       .then(self.skipWaiting())
-//   );
-// });
-// 
-// const fetchConfig = () =>
-//   fetch("config.json", { cache: "reload" })
-//     .then(r => r.json());
-// 
-// self.addEventListener("activate", e => {
-// //   caches.open(cache_name)
-// //     .then(cache => cache.add(offline_url))
-//   if (self.registration.navigationPreload)
-//     e.waitUntil(self.registration.navigationPreload.enable())
-// });
-// 
-// self.addEventListener("fetch", e => {
-//   e.respondWith((async () => {
-//     const offline = async () => (await caches.open(cache_name))
-//         .match(offline_url);
-//     const r = async () => (await e.preloadResponse) || fetch(e.request);
-// 
-//     if (e.request.mode != "navigate")
-//       return r();
-// 
-//     try {
-//       const cfg = await fetchConfig()
-//       if (cfg.default.online === false)
-//         return offline();
-//       return await r();
-//     } catch (e) {
-//       return offline();
-//     }
-//   })());
-// });
-
 const cache_name = "cache";
 const cache_urls = [
   "/",
@@ -62,12 +20,13 @@ self.addEventListener("activate", e => {
 self.addEventListener("fetch", e => {
   e.respondWith((e.preloadResponse || Promise.resolve(null))
     .then(r => r || fetch(e.request))
-    .then(r => r.status == 200 ? r : null)
-    .catch(() => null)
+    // .then(r => r.status == 200 ? r : null)
+    .catch((...args) => null)
     .then(r => {
       if (!r) {
         return caches.open(cache_name)
           .then(cache => cache.match(e.request))
+          .then(match => match || r)
       }
       caches.open(cache_name)
         .then(cache => {
